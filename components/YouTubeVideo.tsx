10421cb91843541b258677c5ebcc3a9d882c60d2
Filip Hric <filip@filiphric.com>
2023-10-09 16:06:48 +0200

    migrate debugger and debugging docs

--- components/YouTubeVideo.tsx
+++ components/YouTubeVideo.tsx
@@ -6,7 +6,7 @@ interface YouTubeVideoProps {
 
 const YouTubeVideo: React.FC<YouTubeVideoProps> = ({ src }) => {
   return (
-    <div className="relative h-0 mt-5 pb-[56.25%]">
+    <div className="relative pb-[56.25%] mt-5 h-0">
       <iframe src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className="absolute top-0 left-0 w-full h-full"></iframe>
     </div>
   );
