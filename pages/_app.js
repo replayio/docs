754382ea62b8ac55a269ab0d31a283d7d08c7673
Filip Hric <filip@filiphric.com>
2023-10-13 11:22:04 +0200

    add umami analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script async src="https://analytics.eu.umami.is/script.js" data-website-id="9dab9357-6fa2-48ab-966a-82c4e1bb67fe" />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
