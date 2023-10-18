5e9981dfd5cd7c3a9d326c6f865eb51a556ebdee
Filip Hric <filip@filiphric.com>
2023-10-18 15:04:31 +0200

    setup proper seo tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import LogosReplay from './icons/LogosReplay'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <LogosReplay width={100} />,
@@ -14,14 +15,33 @@ const config: DocsThemeConfig = {
   footer: {
     text: '® 2023 Replay, All rights reserved.',
   },
-  head: (
-    <>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-    </>
-  )
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.replay.io' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Replay.io docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Replay.io is a tool that makes debugging easy. It lets you record your web app and retroactively inspect it using print statements and browser DevTools.'}
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </>
+    )
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Replay.io docs'
+    }
+  }
 }
 
 export default config
