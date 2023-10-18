d2490a298b13c1173a1279881953457046349d52
Filip Hric <filip@filiphric.com>
2023-10-18 14:24:49 +0200

    fix logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <>
+  return (
+  <ThemeProvider>
     <Script async src="https://analytics.eu.umami.is/script.js" data-website-id="9dab9357-6fa2-48ab-966a-82c4e1bb67fe" />
     <Component {...pageProps} />
-  </>
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
