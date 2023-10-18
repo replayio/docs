63191477702b32b0938ad044321e546a5a4f0d5b
Filip Hric <filip@filiphric.com>
2023-10-18 13:42:23 +0200

    fix broken links, typos and add redirects

--- next.config.js
+++ next.config.js
@@ -3,24 +3,4 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra({
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/getting-started/what-is-replay-io',
-        permanent: true,
-      },
-      {
-        source: '/test-suites',
-        destination: '/test-suites/test-runners',
-        permanent: true,
-      },
-      {
-        source: '/resources',
-        destination: '/resources/get-help',
-        permanent: true,
-      }
-    ];
-  },
-});
+module.exports = withNextra({});
