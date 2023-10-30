7bd947099a463d271073ce6b4b60a4a16c839848
Filip Hric <filip@filiphric.sk>
2023-10-30 07:20:48 +0100

    add new test suites page

---
+++ icons/HeroiconsCommandLine.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function HeroiconsCommandLine(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m6.75 7.5l3 2.25l-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z"></path></svg>
+  )
+}
+export default HeroiconsCommandLine
---
+++ pages/test-suites/Features.tsx
@@ -0,0 +1,53 @@
+import Bug from '../../icons/Bug'
+import HeroiconsCommandLine from '../../icons/HeroiconsCommandLine'
+import MaterialSymbolsFlaky from '../../icons/MaterialSymbolsFlaky'
+import MagnifyingApps from '../../icons/MagnifyingApps'
+
+const items = [
+  {
+    name: 'See what happened on CI',
+    description:
+      'Tests fail on CI but pass locally? Recording a replay of your tests will give you insight you needed.',
+    icon: HeroiconsCommandLine,
+  },
+  {
+    name: 'Debug with ease',
+    description:
+      'Finding a root cause is much easier whan your test flake is recorded in a replay. No more guessing games. Examine the test flake at the very place it happened.',
+    icon: Bug,
+  },
+  {
+    name: 'Goodbye flaky tests',
+    description:
+      'With Replay DevTools you dive deeper than with any other tool. Test flakiness is no longer a mystery, but something that can be found and fixed.',
+    icon: MaterialSymbolsFlaky,
+  },
+  {
+    name: 'Explore both test and app code',
+    description:
+      'Flakiness can come from anywhere - both the app and the test. You cannot make them stable by ignoring one or the other. Replay allows you to take them both into consideration',
+    icon: MagnifyingApps,
+  },
+]
+
+export default function Features() {
+  return (
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto mt-8 max-w-2xl sm:mt-10 lg:mt-12 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {items.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-pink-600">
+                    <feature.icon className="h-6 w-6 jasn'text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+  )
+}
