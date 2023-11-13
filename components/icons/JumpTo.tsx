636305bb353ee5bbba1658421abab2cbb24b5b1c
Filip Hric <filip@filiphric.sk>
2023-11-13 16:48:57 +0100

    update cypress panel doc

---
+++ components/icons/JumpTo.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function JumpTo(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}><path d="M0 0h24v24H0z" fill="none"></path><path d="M19.5012 3C18.6769 3 18.0025 3.675 18.0025 4.5V19.5C18.0025 20.325 18.6769 21 19.5012 21C20.3256 21 21 20.325 21 19.5V4.5C21 3.675 20.3256 3 19.5012 3ZM14.0158 13.23L5.36805 19.335C4.3788 20.04 3 19.32 3 18.105V5.895C3 4.68 4.3638 3.975 5.36805 4.665L14.0158 10.77C14.8701 11.37 14.8701 12.63 14.0158 13.23Z"></path></svg>
+  )
+}
+export default JumpTo
