49d4fca65ab0c6280ff3a4374941d2e2c4b9db96
Filip Hric <filip@filiphric.sk>
2023-11-14 16:20:56 +0100

    add custom replay code theme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const fs = require('fs')
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      // VSCode theme or built-in Shiki theme, see Shiki documentation for more information
+      theme: JSON.parse(
+        fs.readFileSync('./public/assets/shiki-theme.json', 'utf8')
+      )
+    }
+  }
 });
 
 module.exports = withNextra({});
