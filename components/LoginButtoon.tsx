aadd5a024230424a2a47d819a1c6c50462e9d68d
Filip Hric <filip@filiphric.sk>
2024-02-29 15:11:36 +0100

    rename signup to login

--- components/LoginButtoon.tsx
+++ components/LoginButtoon.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SignupButton: React.FC = () => {
+const LoginButtoon: React.FC = () => {
   return (
     <a href="https://app.replay.io">
       <button className='inline-block bg-pink-500 px-5 py-1.5 rounded-md text-sm font-semibold'>
-      Sign up
+      Log in
       </button>
     </a>
   );
 };
 
-export default SignupButton;
+export default LoginButtoon;
