63901c21f293f1477b131a07d629d9384a504a1d
Josh Morrow <josh@jcmorrow.com>
2024-03-26 12:52:02 -0700

    Delete links to recordings
    These rot over time and right now they are all broken basically.

--- components/LoginButtoon.tsx
+++ components/LoginButtoon.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const LoginButtoon: React.FC = () => {
   return (
     <a href="https://app.replay.io">
-      <button className='inline-block bg-pink-500 px-5 py-1.5 rounded-md text-sm font-semibold'>
-      Log in
+      <button className="inline-block rounded-md bg-pink-500 px-5 py-1.5 text-sm font-semibold">
+        Log in
       </button>
     </a>
   );
