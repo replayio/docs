f327c78eb10e0163bbff32590dd5d84d3da2e765
Ryan Duffy <ryan@replay.io>
2024-04-04 12:02:39 -0700

    Add auto-importer for custom tags
    To use it, you have to add the import to components/index.tsx so it is exposed.
    
    We could use dynamic imports from webpack or glob over all the files in that directory when the server starts up but that does not seem worth the effort.
    
    Personally, I do not think this PR is adding much value but putting it up for consideration

--- src/markdoc/tags.js
+++ src/markdoc/tags.js
@@ -1,9 +1,23 @@
-import { Callout } from '@/components/Callout'
-import { QuickLink, QuickLinks } from '@/components/QuickLinks'
-import Video from '@/components/Video'
 import React from 'react'
 
+import components from '@/components'
+
+function toSnakeCase(str) {
+  return (str[0] + str.substr(1).replace(/([A-Z])/g, '-$1')).toLowerCase()
+}
+
+// Import all the components and generate tag stubs for each.
+// If you want to expose attributes, you have to override the entry
+// with the configuration you need.
+const componentTags = Object.fromEntries(
+  Object.keys(components).map((key) => [
+    toSnakeCase(key),
+    { render: components[key] },
+  ]),
+)
+
 const tags = {
+  ...componentTags,
   callout: {
     attributes: {
       title: { type: String },
@@ -14,7 +28,7 @@ const tags = {
         errorLevel: 'critical',
       },
     },
-    render: Callout,
+    render: components.Callout,
   },
   figure: {
     selfClosing: true,
@@ -31,19 +45,16 @@ const tags = {
       </figure>
     ),
   },
-  'quick-links': {
-    render: QuickLinks,
-  },
-  'video': {
-    render: Video,
+  video: {
+    render: components.Video,
     selfClosing: true,
     attributes: {
-      src: { type: String }
-    }
+      src: { type: String },
+    },
   },
   'quick-link': {
     selfClosing: true,
-    render: QuickLink,
+    render: components.QuickLink,
     attributes: {
       title: { type: String },
       description: { type: String },
