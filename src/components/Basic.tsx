c61d02f727bcf946aecce4da17b5b8734bbe029a
Jason Laster <jason.laster.11@gmail.com>
2024-04-05 14:52:50 -0700

    shift icon to negative space

--- src/components/Basic.tsx
+++ src/components/Basic.tsx
@@ -8,7 +8,7 @@ export function Basic({
     children: React.ReactNode 
     icon: React.ComponentProps<typeof Icon>['icon'] }) {
   return (
-    <div className="flex">
+    <div className="flex" style={{marginLeft: '-52px'}}>
       <div className="mr-4 mt-12 flex-shrink-0">
         <Icon icon={icon} className="h-9 w-9 fill-slate-50 dark:fill-gray-950" />
       </div>
