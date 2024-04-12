e7b5929a6e614c8bff0aea034e33997dff72ace9
Jason Laster <jason.laster.11@gmail.com>
2024-04-12 07:24:10 -0700

    small visual hlanding page tweaks

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,27 @@
-import Link from "next/link";
-import { Icon, icons } from "./Icon";
+import Link from 'next/link'
+import { Icon, icons } from './Icon'
 
 export interface CardAttributes {
-  title: string, 
-  content: string,
-  icon: keyof typeof icons,
+  title: string
+  content: string
+  icon: keyof typeof icons
   href: string
 }
 
-export default function Card({
-  title, 
-  content,
-  icon,
-  href
-}: CardAttributes) {
-
-return (
-  <Link href={href}>
-    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="w-full h-40 bg-gradient-to-br from-sky-500 to-pink-300 flex justify-center items-center">
-        <Icon icon={icon} className="w-20 h-20 opacity-95 text-white" />
+export default function Card({ title, content, icon, href }: CardAttributes) {
+  return (
+    <Link href={href}>
+      <div className="max-w-xs overflow-hidden rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl">
+        <div className="flex h-40 w-full items-center justify-center bg-gradient-to-br from-sky-500 to-pink-300">
+          <Icon icon={icon} className="h-20 w-20 text-white opacity-95" />
+        </div>
+        <div className="px-6 py-9 dark:bg-white/5">
+          <div className="mb-2 text-xl font-bold dark:text-slate-100">
+            {title}
+          </div>
+          <p className="dark:text-slate-300">{content}</p>
+        </div>
       </div>
-      <div className="px-6 py-9 dark:bg-white/5">
-        <div className="font-bold text-xl mb-4">{title}</div>
-        <p className="text-gray-500">
-          {content}
-        </p>
-      </div>
-    </div>
-  </Link>   
+    </Link>
   )
 }
--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -1,34 +1,34 @@
-import clsx from "clsx";
-import { CardAttributes } from "./Card";
-import { Icon } from "./Icon";
+import clsx from 'clsx'
+import { CardAttributes } from './Card'
+import { Icon } from './Icon'
 
 export default function MiniCard({
-  title, 
+  title,
   content,
   icon,
   href,
 }: CardAttributes) {
-
-return (
+  return (
     <div>
-      
-          <li key={title} className="col-span-1 flex rounded-md shadow-sm bg-black bg-opacity-30">
-            <div className="flex justify-center items-center p-5">
-              <Icon icon={icon} className="w-8 h-8" />
-            </div>
-            <div className="flex flex-1 items-center justify-between">
-              <div className="flex-1 px-4 py-2 text-sm">
-                <p className="font-medium text-white hover:text-gray-600">
-                  {title}
-                </p>
-                <p>{content}</p>
-              </div>
-              <div className="flex-shrink-0 pr-2">
-                  <Icon icon="chevron" className="w-8 h-8" />
-              </div>
-            </div>
-          </li>
-    
+      <li
+        key={title}
+        className="col-span-1 flex cursor-pointer rounded-md  border border-gray-100 shadow-sm hover:border-gray-200 dark:bg-slate-800"
+      >
+        <div className="flex items-center justify-center p-5">
+          <Icon icon={icon} className="h-8 w-8" />
+        </div>
+        <div className="flex flex-1 items-center justify-between">
+          <div className="flex-1 px-4 py-2 text-sm">
+            <p className="font-semibold text-gray-800 hover:text-gray-900 dark:text-slate-50">
+              {title}
+            </p>
+            <p className="text-gray-600 dark:text-slate-300">{content}</p>
+          </div>
+          <div className="flex-shrink-0 pr-2 ">
+            <Icon icon="chevron" className="h-8 w-8" />
+          </div>
+        </div>
+      </li>
     </div>
   )
 }
