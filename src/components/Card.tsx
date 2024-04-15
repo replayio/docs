1da43a48bdac009f7be2049b0efe72d9f0e8fc81
Jason Laster <jason.laster.11@gmail.com>
2024-04-14 17:51:23 -0700

    Linear layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,26 +8,18 @@ export interface CardAttributes {
   href: string
 }
 
-export default function Card({
-  title, 
-  content,
-  icon,
-  href
-}: CardAttributes) {
-
-return (
-  <Link href={href}>
-    <div className="max-w-xs rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="w-full h-40 bg-gradient-to-br from-sky-500 to-pink-300 flex justify-center items-center">
-        <Icon icon={icon} className="w-20 h-20 opacity-95 text-white" />
+export default function Card({ title, content, icon, href }: CardAttributes) {
+  return (
+    <Link href={href}>
+      <div className="max-w-xs overflow-hidden rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl">
+        <div className="flex h-40 w-full items-center justify-center bg-gradient-to-br from-sky-500 to-pink-300">
+          <Icon icon={icon} className="h-20 w-20 text-white opacity-95" />
+        </div>
+        <div className="px-6 py-6 dark:bg-slate-700/50">
+          <div className="text-md mb-1 font-bold">{title}</div>
+          <p className="text-sm text-gray-700 dark:text-gray-300">{content}</p>
+        </div>
       </div>
-      <div className="px-6 py-9 dark:bg-gray-950/50">
-        <div className="font-bold text-xl mb-4">{title}</div>
-        <p className="dark:text-gray-300 text-gray-700">
-          {content}
-        </p>
-      </div>
-    </div>
-  </Link>   
+    </Link>
   )
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-import { Hero } from '@/components/Hero'
 import { Logo, Logomark } from '@/components/Logo'
 import { MobileNavigation } from '@/components/MobileNavigation'
 import { Navigation } from '@/components/Navigation'
@@ -20,9 +19,17 @@ function GitHubIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-function DiscordIcon(props: React.ComponentPropsWithoutRef<'svg'>)  {
+function DiscordIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"></path></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"></path>
+    </svg>
   )
 }
 
@@ -43,36 +50,44 @@ function Header() {
   return (
     <header
       className={clsx(
-        'sticky top-0 z-50 px-4 py-5 shadow-gray-900/5 transition duration-500 sm:px-6 sm:py-5 lg:px-8 shadow-none',
+        'sticky top-0 z-50 px-12 py-5 shadow-none shadow-gray-900/5 transition duration-500 sm:px-12 sm:py-5 lg:px-12',
         isScrolled
-          ? 'dark:bg-gray-900/95 backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-gray-900/75 [@supports(backdrop-filter:blur(0))]:bg-white/45'
+          ? 'backdrop-blur dark:bg-gray-900/95 [@supports(backdrop-filter:blur(0))]:bg-white/45 dark:[@supports(backdrop-filter:blur(0))]:bg-gray-900/75'
           : 'bg-transparent',
       )}
     >
       <div>
-      <div className='flex flex-none flex-wrap items-center justify-between'>
-        <div className="mr-6 flex lg:hidden">
-          <MobileNavigation />
+        <div className="flex flex-none flex-wrap items-center justify-between">
+          <div className="mr-6 flex lg:hidden">
+            <MobileNavigation />
+          </div>
+          <div className="relative flex flex-grow basis-0 items-center">
+            <Link href="/" aria-label="Home page">
+              <Logomark className="h-7 w-7 lg:hidden" />
+              <Logo className="hidden h-6 w-auto fill-gray-700 lg:block dark:fill-sky-100" />
+            </Link>
+          </div>
+          <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
+            <Search />
+          </div>
+          <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
+            <ThemeSelector className="relative z-10" />
+            <Link
+              href="https://github.com/replayio"
+              className="group"
+              aria-label="GitHub"
+            >
+              <GitHubIcon className="h-6 w-6 fill-gray-400 group-hover:fill-gray-500 dark:group-hover:fill-gray-300" />
+            </Link>
+            <Link
+              href="https://replay.io/discord"
+              className="group"
+              aria-label="Discord"
+            >
+              <DiscordIcon className="h-6 w-6 fill-gray-400 group-hover:fill-gray-500 dark:group-hover:fill-gray-300" />
+            </Link>
+          </div>
         </div>
-        <div className="relative flex flex-grow basis-0 items-center">
-          <Link href="/" aria-label="Home page">
-            <Logomark className="h-9 w-9 lg:hidden" />
-            <Logo className="hidden h-9 w-auto fill-gray-700 lg:block dark:fill-sky-100" />
-          </Link>
-        </div>
-        <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
-          <Search />
-        </div>
-        <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
-          <ThemeSelector className="relative z-10" />
-          <Link href="https://github.com/replayio" className="group" aria-label="GitHub">
-            <GitHubIcon className="h-6 w-6 fill-gray-400 group-hover:fill-gray-500 dark:group-hover:fill-gray-300" />
-          </Link>
-          <Link href="https://replay.io/discord" className="group" aria-label="Discord">
-            <DiscordIcon className="h-6 w-6 fill-gray-400 group-hover:fill-gray-500 dark:group-hover:fill-gray-300" />
-          </Link>
-        </div>
-      </div>
       </div>
     </header>
   )
@@ -84,27 +99,31 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex w-full flex-col">
-      <div className='top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none fixed -z-10'>
-        <div className="w-full flex-none flex justify-end bg-gradient-to-tr from-white dark:from-gray-800 dark:via-sky-600 via-white to-pink-500 h-screen dark:opacity-10 opacity-5"></div>
+      <div className="pointer-events-none fixed inset-x-0 top-0 -z-10 flex justify-center overflow-hidden">
+        <div className="flex h-screen w-full flex-none justify-end bg-gradient-to-tr from-white via-white to-pink-500 opacity-5 dark:from-gray-800 dark:via-sky-600 dark:opacity-10"></div>
       </div>
       <Header />
 
-      {isHomePage && <Hero />}
-
-      <div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12">
-        <div className="hidden lg:relative lg:block lg:flex-none">
-          {!isHomePage && 
-          <>
-            <div className="absolute inset-y-0 right-0 w-[50vw] bg-gray-50 dark:hidden" />
-            <div className="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-gray-800 dark:block" />
-            <div className="absolute bottom-0 right-0 top-28 hidden w-px bg-gray-800 dark:block" />
-            <div className="sticky top-[4.75rem] -ml-0.5 h-[calc(100vh-4.75rem)] overflow-y-auto overflow-x-hidden py-16 pl-0.5 pr-2 w-72 xl:pr-2">
-            <Navigation />
-          </div>
-          </>
+      <div className="relative mx-auto flex w-full  flex-auto  sm:px-2 lg:px-8 xl:px-12">
+        <div className="mr-12 hidden lg:relative lg:block lg:flex-none">
+          {
+            <>
+              <div className="absolute inset-y-0 right-0 w-[50vw] bg-gray-50 dark:hidden" />
+              <div className="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-gray-800 dark:block" />
+              <div className="absolute bottom-0 right-0 top-28 hidden w-px bg-gray-800 dark:block" />
+              <div className="sticky top-[4.75rem] -ml-0.5 h-[calc(100vh-4.75rem)] w-72 overflow-y-auto overflow-x-hidden py-16 pl-0.5 pr-2 xl:pr-2">
+                <Navigation />
+              </div>
+            </>
           }
         </div>
-        {children}
+
+        <div
+          data-test-id="main-content"
+          className="flex w-full flex-auto grow flex-col items-center justify-center"
+        >
+          <div className="flex w-full grow flex-row ">{children}</div>
+        </div>
       </div>
     </div>
   )
