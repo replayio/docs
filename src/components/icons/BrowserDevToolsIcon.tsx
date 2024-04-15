f9df006c7edf7b7126b64c3790e4c633e9259818
Jon Bell <github@lot23.com>
2024-04-15 15:21:59 +1200

    Comparisons icons

--- src/components/icons/BrowserDevToolsIcon.tsx
+++ src/components/icons/BrowserDevToolsIcon.tsx
@@ -1,9 +1,19 @@
 export function BrowserDevToolsIcon() {
   return (
-    <path
-      fill="currentColor"
-      d="M13 19v-3h8v3zm-4.5-6L2.47 7h4.24l4.96 4.95c.58.59.58 1.55 0 2.12L6.74 19H2.5z"
-    ></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z"
+      />
+    </svg>
   )
 }
 
