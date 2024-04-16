55b22dc963f7a38e1c53a17b48d7ccb8d051a8ba
Jon Bell <github@lot23.com>
2024-04-16 15:00:03 +1200

    Adding mixpanel

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -2,17 +2,26 @@
 
 import { useEffect } from 'react'
 import LogRocket from 'logrocket'
+import mixpanel from 'mixpanel-browser'
 
 export default function Analytics() {
   useEffect(() => {
     LogRocket.init('4sdo4i/replay-docs')
+    mixpanel.init('ffaeda9ef8fb976a520ca3a65bba5014')
+    mixpanel.track('Loaded docs.replay.io')
   }, [])
 
   return (
-    <script
-      async
-      src="https://analytics.umami.is/script.js"
-      data-website-id="9dab9357-6fa2-48ab-966a-82c4e1bb67fe"
-    ></script>
+    <>
+      <script
+        async
+        src="https://analytics.umami.is/script.js"
+        data-website-id="9dab9357-6fa2-48ab-966a-82c4e1bb67fe"
+      ></script>
+      <script
+        async
+        src="https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"
+      ></script>
+    </>
   )
 }
