c55b9675b7f1ccbc1b02e6f13361cad0dd3ccbe9
Jason Laster <jason.laster.11@gmail.com>
2024-04-17 12:57:28 -0700

    fix quick links border

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -40,8 +40,8 @@ export function QuickLink({
   icon: React.ComponentProps<typeof Icon>['icon']
 }) {
   return (
-    <div className="group relative rounded-xl border bg-slate-50 dark:bg-slate-800 ">
-      <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100  dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
+    <div className="group relative rounded-xl border border-gray-200 dark:border-gray-700">
+      <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />{' '}
       <div className="relative overflow-hidden rounded-xl p-6">
         <Icon icon={icon} className="h-8 w-8" />
         <h3 className=" font-display text-base text-gray-900 dark:text-white">
