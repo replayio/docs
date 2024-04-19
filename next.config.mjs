b62010b16c37ca1e2b23a95d665c81752d3ff90d
Jason Laster <jason.laster.11@gmail.com>
2024-04-18 22:18:05 -0700

    Add prod sourcemaps

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  productionBrowserSourceMaps: true,
 
   async redirects() {
     return [
