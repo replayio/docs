09b35eac812c534448153de8db35dd0cd7a56147
Corbin Crutchley <git@crutchcorn.dev>
2024-05-06 17:38:19 -0700

    docs: add accordion component with quickstart FAQ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,20 @@ export default {
       '9xl': ['8rem', { lineHeight: '1' }],
     },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       screens: {
         sm: '750px',
       },
