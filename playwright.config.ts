14597d2804477f39971ef3fccde514655ea9e2aa
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-05-22 14:35:15 +0200

    further update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,20 +1,17 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test'
-import { devices as replayDevices } from '@replayio/playwright'
-import dotenv from 'dotenv';
-dotenv.config();
+import { devices as replayDevices, replayReporter } from '@replayio/playwright'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const config: PlaywrightTestConfig = {
   use: {
-    baseURL: process.env.CI ? process.env.BASE_URL : "http://localhost:3000"
+    baseURL: process.env.CI ? process.env.BASE_URL : 'http://localhost:3000',
   },
   reporter: [
-    [
-      '@replayio/playwright/reporter',
-      {
-        apiKey: process.env.REPLAY_API_KEY,
-        upload: true,
-      },
-    ],
+    replayReporter({
+      apiKey: process.env.REPLAY_API_KEY,
+      upload: true,
+    }),
     ['line'],
   ],
   projects: [
