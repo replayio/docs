a0077c7802ec3200a6e14f06eaa62bd66e749f89
Jason Laster <jason.laster.11@gmail.com>
2024-05-22 18:01:30 -0400

    update playwright config

--- tests/quickstart.spec.ts
+++ tests/quickstart.spec.ts
@@ -1,11 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 test('home page redirects to quickstart guide', async ({ page }) => {
-
   await page.goto('/')
   await page.getByText('Get started with our Quickstart Guide').click()
   await expect(page).toHaveURL(/.*\/quickstart/)
-  const heading = page.getByText('Quickstart guide', {exact: true})
+  const heading = page.getByText('Quickstart guide', { exact: true })
   await expect(heading).toBeVisible()
-
 })
