c6dfc9c37e24c3eec77c47c08141b8d6f0f7d78c
Jason Laster <jason.laster.11@gmail.com>
2024-05-30 12:13:53 -0700

    fix zoom test (fyi it passed in ci because of redirects)

--- tests/zoomPlayer.spec.ts
+++ tests/zoomPlayer.spec.ts
@@ -1,18 +1,16 @@
 import { test, expect } from '@playwright/test'
 
 test('player zoom in', async ({ page }) => {
-
-  await page.goto('/test-runners/playwright/debugging-tests')
+  await page.goto('/reference/test-runners/playwright/debugging-tests')
 
   const image = page.locator('[data-testid="image-with-zoom"]').first()
-  await image.scrollIntoViewIfNeeded() 
+  await image.scrollIntoViewIfNeeded()
   await image.click()
-  
+
   const playerZoomedIn = page.locator('[data-testid="zoomed-in-player"]')
-  await playerZoomedIn.scrollIntoViewIfNeeded() 
+  await playerZoomedIn.scrollIntoViewIfNeeded()
   await expect(playerZoomedIn).toHaveCount(1)
-  
+
   await image.click()
   await expect(playerZoomedIn).toHaveCount(0)
-
 })
